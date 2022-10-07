77d1801a234acc8ea91653bee3d865147ca7d702
Dominic Winter-Pacheco <domiwint423@ed.amdsb.ca>
2022-10-07 10:25:02 -0400

    Add project files.

---
+++ amIPositiveNo/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace amIPositiveNo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterLabel = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enterLabel
+            // 
+            this.enterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterLabel.Location = new System.Drawing.Point(130, 98);
+            this.enterLabel.Name = "enterLabel";
+            this.enterLabel.Size = new System.Drawing.Size(179, 54);
+            this.enterLabel.TabIndex = 0;
+            this.enterLabel.Text = "Input An Integer ";
+            this.enterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(351, 119);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 1;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(351, 170);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(100, 49);
+            this.enterButton.TabIndex = 2;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(156, 279);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(100, 23);
+            this.outputLabel.TabIndex = 3;
+            // 
+            // outputLabel2
+            // 
+            this.outputLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel2.Location = new System.Drawing.Point(156, 346);
+            this.outputLabel2.Name = "outputLabel2";
+            this.outputLabel2.Size = new System.Drawing.Size(100, 23);
+            this.outputLabel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 449);
+            this.Controls.Add(this.outputLabel2);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.enterLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label enterLabel;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label outputLabel2;
+    }
+}
+
